2a42f3ca17b16e46522b268d5238cd58e3e07bae
tareq <tareqsabra949@gmail.com>
2024-10-05 17:41:03 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+require("dotenv").config(); // Load environment variables
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+const fs = require("fs");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds, // Guild-related events
+    GatewayIntentBits.GuildMessages, // Messages from guilds
+    GatewayIntentBits.MessageContent, // Privileged intent for message content
+  ],
+});
+
+client.commands = new Collection();
+const prefix = process.env.PREFIX;
+
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("messageCreate", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
+
+  const command = client.commands.get(commandName);
+  if (!command) return;
+
+  try {
+    command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply("There was an error trying to execute that command!");
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
