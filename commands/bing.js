1fc885cc3eb6b9ec8e2571de5e10e2632e0b3c85
tareq <tareqsabra949@gmail.com>
2024-10-05 17:41:36 +0300

    fixed

--- commands/bing.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: "ping",
-  description: "Replies with Pong!",
-  execute(message, args) {
-    message.channel.send("Pong :qadomi2: :qadomi2: :qadomi2:");
-  },
-};
--- commands/slurb.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: "suck",
-  description: "sucks",
-  execute(message, args) {
-    message.channel.send("slurping your Big 🍆");
-  },
-};
